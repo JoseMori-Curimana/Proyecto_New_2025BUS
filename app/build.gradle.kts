ddf170385c0782c88b320bda93ca21b3254db236
Jans sinarahua torrese <jansxxx.24@gmail.com>
2025-07-09 21:15:49 -0500

    cambios de bd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,18 +3,16 @@ plugins {
     id("com.google.gms.google-services")
 }
 
-
 android {
-    namespace = "com.example.rutas"
+    namespace = "com.example.proyecto_new_2025bus"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.rutas"
+        applicationId = "com.example.proyecto_new_2025bus"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -27,36 +25,26 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    buildToolsVersion = "35.0.0"
 }
 
 dependencies {
-    implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation ("com.google.maps.android:android-maps-utils:2.2.5")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
-    implementation("com.google.firebase:firebase-database")
+    // Core Android
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
     // Google Maps
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 
-
-
-// Firebase Realtime Database
+    // Firebase Realtime Database
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-analytics")
 
-    apply(plugin = "com.google.gms.google-services")
-
-
+    // Test
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
