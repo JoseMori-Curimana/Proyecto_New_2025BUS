625f2f495814afe6f31aa2cd19f0c36793e988cc
Jans sinarahua torrese <jansxxx.24@gmail.com>
2025-07-12 02:27:11 -0500

    agregando menu

--- app/src/main/java/com/example/proyecto_new_2025bus/HomeChoferActivity.java
+++ app/src/main/java/com/example/proyecto_new_2025bus/HomeChoferActivity.java
@@ -1,5 +1,6 @@
 package com.example.proyecto_new_2025bus;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class HomeChoferActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_choferprincipal);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_inicio:
+                    startActivity(new Intent(this, chofer.class));
+                    return true;
+
+        case R.id.nav_asientos:
+            startActivity(new Intent(this, AsistenciaActivity.class));
+            return true;
+
+        case R.id.nav_perfil:
+            startActivity(new Intent(this, HistorialActivity.class));
+            return true;
+
+
+                default:
+                    return false;
+            }
+        });
+
     }
 }
